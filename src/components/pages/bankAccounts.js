5111e8a429ed7ae026e93cbbe38844f7cebe80c3
danieldeoc <danieldeoc@gmail.com>
2023-02-27 10:14:37 +0000

    Bank accounts basic setup added

--- src/components/pages/bankAccounts.js
+++ src/components/pages/bankAccounts.js
@@ -2,6 +2,25 @@ import React, { useEffect, useState } from "react";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore, collection, getDocs, query, serverTimestamp, addDoc, orderBy, doc, updateDoc, deleteDoc } from "firebase/firestore"
+    /* In the bank account register it will:
+        Add new bank accounts:
+            {
+                id:
+                Name:
+                InitialDeposit: $
+                Currency:
+                PaymentMethods: []
+                Status: true|false // if open or closed
+                Movments: // a collection of movments containing
+                    {
+                        date:
+                        movmentType:
+                        operatingValue:
+                        payMethod:
+                        position:
+                    }
+            }
+    */
 
 function BankAccounts(){
 
@@ -26,76 +45,119 @@ function BankAccounts(){
     const [initialDeposit, setInitialDeposit] = useState(0);
     const [bankCurrency, setBankCurrency] = useState("Euro");
     const [bankStatus, setBankStatus] = useState(true);
-    const [bankPaymentsMethods, setBankPaymentsMethods] = useState([]);
+    const [bankPaymentsMethods, setBankPaymentsMethods] = useState(["Account Debit"]);
+    
+    /* ############################# */
+    // Interface constants
+    const [listBankAccounts, setListBankAccounts] = useState(["Wait..."]);
 
-
+    /* ############################# */
+    // Add an account
     function addBankAccount(){
         addDoc(colRef, {
             Name: bankName,
             InitialDeposit: initialDeposit,
+            CurrentFunds: initialDeposit,
             Currency: bankCurrency,
             PaymentMethods: bankPaymentsMethods,
             Status: bankStatus,
             CreatedAt: serverTimestamp()
         }).then(() => {
-            
+            setBankName("");
+            setInitialDeposit(0.00);
+            setBankCurrency("Euro");
+            setBankPaymentsMethods(["Account Debit"]);
+            document.getElementById("bankCurrency").value = "Euro";
+            document.getElementById("cash").checked = false;            
+            document.getElementById("accountdebit").checked = true;
+            document.getElementById("debitCard").checked = true;
+            getBankAccounts();
             console.log("New Bank Created")
         }).catch( err => console.log(err))
     }
 
+    /* ############################# */
+    // Payment Methods 
+    function payMethodsCheck(){
+        let payMethods = []
+        const payBox = document.querySelector(".paymethodsOptions");
+        const cash = document.getElementById("cash").checked;
+        if(cash === true ? payMethods.push("cash") : "");
+        const accountDebit = document.getElementById("accountdebit").checked;
+        if(accountDebit === true ? payMethods.push("Account Debit") : "");
+        const debitCard = document.getElementById("debitCard").checked;
+        if(debitCard === true ? payMethods.push("Debit Card") : "");
+        setBankPaymentsMethods(payMethods)
+    }
 
-    /* In the bank account register it will:
-        Add new bank accounts:
-            {
-                id:
-                Name:
-                InitialDeposit: $
-                Currency:
-                PaymentMethods: []
-                Status: true|false // if open or closed
-                Movments: // a collection of movments containing
-                    {
-                        date:
-                        movmentType:
-                        operatingValue:
-                        payMethod:
-                        position:
-                    }
-            }
-    */
+    /* ############################# */
+    // Get Bank Accounts
+    function getBankAccounts(){
+        const bankAccountLIst = [];
+        const queryList = query(colRef, orderBy('Name', 'asc')) 
+        getDocs(queryList).then( (snapshot) => {
+            snapshot.docs.forEach( (doc) => {
+                bankAccountLIst.push( {...doc.data(), id: doc.id} )
+            });
+            setListBankAccounts(
+                bankAccountLIst.map( (key, i) => (
+                    <li key={i}>
+                        {key.Name}
+                        {key.Currency}
+                        {key.InitialDeposit}
+                        {key.CurrentFunds}
+                        {key.PaymentMethods}
+                        {key.Status}
+                        {Date(key.CreatedAt)}
+                        <i onClick={ () => { deletBankAccount(key.id) } }>X</i>
+                    </li>
+                ))
+            );
+                
+        });
+    }
 
-            function multiOptionsSet(value){
-                console.log(value)
-                //setBankPaymentsMethods()
-            }
+    /* ############################# */
+    // Delet Bank Accounts
+    function deletBankAccount(id){
+        const docRef = doc(db, collectionRef, id);
+        deleteDoc(docRef)
+            .then( () => {
+                console.log("deleted");
+                getBankAccounts();
+            }).catch( (err) => console.log(err))
+    }
+
+    useEffect( () => {
+        getBankAccounts();
+    }, [])
 
     return(
         <>
             Bank Accounts
             {bankName} / {bankCurrency} / {initialDeposit} / {bankPaymentsMethods} 
             <div className="addBankAccount">
-                Bank: <input type="text" onChange={ (e) => { setBankName(e.target.value) } } />
-                Initial Deposit: <input type="number" onChange={ (e) => { setInitialDeposit(e.target.value) } } /> 
-                Currency: <select  onChange={ (e) => { setBankCurrency(e.target.value) } }>
+                Bank: <input type="text" value={bankName} onChange={ (e) => { setBankName(e.target.value) } } />
+                Initial Deposit: <input type="number" value={initialDeposit} onChange={ (e) => { setInitialDeposit(e.target.value) } } /> 
+                Currency: <select id="bankCurrency" onChange={ (e) => { setBankCurrency(e.target.value) } }>
                     <option>Euro</option>
                     <option>Real</option>
                     <option>Dolar</option>
                     <option>Libra</option>
                 </select>
                 PaymentMethods:
-                <select multiple  onChange={ (e) => { multiOptionsSet(e.target.value) } }>
-                    <option>Cash</option>
-                    <option>Account Debit</option>
-                    <option>Debit Card</option>
-                </select>
+                <div className="paymethodsOptions">
+                    <input id="cash" type="checkbox" onClick={payMethodsCheck} /> Cash
+                    <input id="accountdebit" defaultChecked="true" name="accountdebit" type="checkbox"  onClick={payMethodsCheck} /> Account Debit
+                    <input id="debitCard" name="debitcard" type="checkbox"  onClick={payMethodsCheck} /> Debit Card
+                </div>
             </div>
-        
-        
+            <button onClick={addBankAccount}>Save</button>
+            <h2>Avaliable Bank Accounts</h2>
+            <ul>
+                {listBankAccounts}
+            </ul>
         </>
-
-
-
-
     )
 }
 
--- src/components/pages/registerExpenses.js
+++ src/components/pages/registerExpenses.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection, getDocs, query, serverTimestamp, addDoc, orderBy, doc, updateDoc, deleteDoc } from "firebase/firestore"
+/* 
+    in order to register a new expenses it will:
+    a) perform a produc catalog search
+    b) register the following information:
+        {
+            Date: data da despesa
+            Product: {A product from the catalog or a new one}
+            Category: {a list or preseted from the product}
+            Price: The value of the product
+            Ammount: How much product
+            Ammount Unit: [un, kg, l]
+            Account: {Bank account list}
+            Account payment Method: {Bank payment method}
+        }
+    On it register it must:
+        a) add a new produc to the catalog if it does not exist
+        b) perform a bank balance update
+        c) update bank current value
 
-
+*/
 function RegisterExpenses(){
     return(
-        <>s</>
+        <>
+            <h1>Expenses</h1>
+
+
+        </>
     )
 }
 
